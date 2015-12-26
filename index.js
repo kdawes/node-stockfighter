cf566d33ba141d2dfa9b087ea662b3d0e28be01f
Kelsey Dawes <kdawes@gmail.com>
2015-12-26 17:41:30 -0600

    add support for the game master / level start stop restart resume status commands

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var request = require('request')
 var promise = require('promise')
-
+// / special
 var config = require('./config/config')
+
 //
 module.exports = api
 function api (opts) {
@@ -10,22 +11,83 @@ function api (opts) {
   }
 }
 
-api.prototype.getApiKey = function () {
-  return config.api_key
-}
-
 function promisifyRequest (options) {
   var P = new promise(function (resolve, reject) {
     request(options, function (e, r, b) {
-      if (e) { console.log('error', e); reject(e); return }
+      if (e) { console.log('promisifyReqest: request: error', e); reject(e); return }
       resolve({error: e, result: r, body: b})
     })
   }).then(function (body) {
     return body
   }).catch(function (err) {
+    console.log('promisifyRequest: error: ', err)
     return err
   })
   return P
+}
+
+api.prototype.levelStart = function (levelName) {
+  var url = 'https://www.stockfighter.io/gm/levels/' + levelName
+  var options = {
+    method: 'POST',
+    url: url,
+    headers: {
+      'X-Starfighter-Authorization': config.api_key
+    }
+  }
+  return promisifyRequest(options)
+}
+
+api.prototype.levelRestart = function (instanceId) {
+  var url = 'https://www.stockfighter.io/gm/instances/' + instanceId + '/restart'
+  var options = {
+    method: 'POST',
+    url: url,
+    headers: {
+      'X-Starfighter-Authorization': config.api_key
+    }
+  }
+  return promisifyRequest(options)
+}
+
+api.prototype.levelStop = function (instanceId) {
+  var url = 'https://www.stockfighter.io/gm/instances/' + instanceId + '/stop'
+  var options = {
+    method: 'POST',
+    url: url,
+    headers: {
+      'X-Starfighter-Authorization': config.api_key
+    }
+  }
+  return promisifyRequest(options)
+}
+
+api.prototype.levelResume = function (instanceId) {
+  var url = 'https://www.stockfighter.io/gm/instances/' + instanceId + '/resume'
+  var options = {
+    method: 'POST',
+    url: url,
+    headers: {
+      'X-Starfighter-Authorization': config.api_key
+    }
+  }
+  return promisifyRequest(options)
+}
+
+api.prototype.levelCheck = function (instanceId) {
+  var url = 'https://www.stockfighter.io/gm/instances/' + instanceId
+  var options = {
+    method: 'GET',
+    url: url,
+    headers: {
+      'X-Starfighter-Authorization': config.api_key
+    }
+  }
+  return promisifyRequest(options)
+}
+
+api.prototype.getApiKey = function () {
+  return config.api_key
 }
 
 api.prototype.cancelBid = function (venueId, stockId, bidId) {
@@ -51,7 +113,6 @@ api.prototype.getAllBidStatus = function (venueId, acountId) {
   }
   return promisifyRequest(options)
 }
-
 // https://api.stockfighter.io/ob/api/venues/:venue/accounts/:account/stocks/:stock/orders
 api.prototype.getAllBidStatusForStock = function (venueId, acountId, stockId) {
   var url = 'https://api.stockfighter.io/ob/api/venues/' + venueId + '/accounts/' + accountId + '/stocks/' + stockId + '/orders'
@@ -89,6 +150,45 @@ api.prototype.getQuote = function (venueId, stockId) {
   return promisifyRequest(options)
 }
 
+// {
+//   "account": "MST92145671",
+//   "venue": "LOBHEX",
+//   "stock": "LPEI",
+//   "qty": 100,
+//   "direction": "buy",
+//   "orderType": "market"
+// }
+api.prototype.sell = function (accountId, venueId, stockId, price, qty, orderType) {
+  var direction = 'sell'
+  var url = 'https://api.stockfighter.io/ob/api/venues/' + venueId + '/stocks/' + stockId + '/orders'
+  var options = {
+    method: 'POST',
+    url: url,
+    json: true,
+    body: {
+      account: accountId,
+      venue: venueId,
+      stock: stockId,
+      price: price,
+      qty: qty,
+      direction: direction,
+      orderType: orderType
+    },
+    headers: {
+      'X-Starfighter-Authorization': config.api_key
+    }
+  }
+  return promisifyRequest(options)
+}
+
+// {
+//   "account": "MST92145671",
+//   "venue": "LOBHEX",
+//   "stock": "LPEI",
+//   "qty": 100,
+//   "direction": "buy",
+//   "orderType": "market"
+// }
 api.prototype.bid = function (accountId, venueId, stockId, price, qty, orderType) {
   var direction = 'buy'
   var url = 'https://api.stockfighter.io/ob/api/venues/' + venueId + '/stocks/' + stockId + '/orders'
@@ -152,6 +252,17 @@ api.prototype.heartbeat = function () {
   var options = {
     method: 'GET',
     url: 'https://api.stockfighter.io/ob/api/heartbeat',
+    headers: {
+      'X-Starfighter-Authorization': config.api_key
+    }
+  }
+  return promisifyRequest(options)
+}
+
+api.prototype.flash = function (instanceId) {
+  var options = {
+    method: 'GET',
+    url: 'https://www.stockfighter.io/gm/instances/' + instanceId,
     headers: {
       'X-Starfighter-Authorization': config.api_key
     }
